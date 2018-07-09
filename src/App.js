d646cf7a3b6c2a121e3448017ddd58098dc4307e
Rhys Woodberry <rhysbrian@gmail.com>
2018-07-09 14:12:57 +1000

    add list to app

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul className="To-Do">
+          <li>Create first Component</li>
+          <li>Create second Component</li>
+          <li>Create third Component</li>
+        </ul>
       </div>
     );
   }
