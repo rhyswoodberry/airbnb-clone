347ecd596e7473bef36328e9d236b2ab2f65ded3
Rhys Woodberry <rhysbrian@gmail.com>
2018-07-11 09:59:57 +1000

    add flat component with style

--- src/components/Flat.js
+++ src/components/Flat.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './Flat.css';
+
 class Flat extends React.Component {
   render() {
+    const title = this.props.flat.price + this.props.flat.priceCurrency + " - " + this.props.flat.name;
+    const style = {
+      backgroundImage: `url('${this.props.flat.imageUrl}')`
+    }
+
     return (
       <div className="flat">
         <div className="flat-picture">
+          {style}
         </div>
         <div className="flat-title">
+          {title}
         </div>
       </div>
     );
