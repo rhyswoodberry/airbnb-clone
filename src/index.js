eef24d794ad44c1e2296c1cfebb12f932bd3612b
Rhys Woodberry <rhysbrian@gmail.com>
2018-07-11 23:34:07 +1000

    render flat with json data

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import Hello from './components/Hello';
+// import registerServiceWorker from './registerServiceWorker';
+// import Hello from './components/Hello';
 
-const root = <div>
-  <Hello firstName="Boris" lastName="Jambon" />
-  <Hello firstName="Garette" lastName="Jambon" />
-  </div>;
-ReactDOM.render(root, document.getElementById('root'));
-registerServiceWorker();
+// const root = <div>
+//   <Hello firstName="Boris" lastName="Jambon" />
+//   <Hello firstName="Garette" lastName="Jambon" />
+//   </div>;
+ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
