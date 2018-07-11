9069157a786949a0b3dd3fe990b900c71b9ae102
Rhys Woodberry <rhysbrian@gmail.com>
2018-07-12 00:14:35 +1000

    add state to app.js

--- src/App.js
+++ src/App.js
@@ -4,26 +4,21 @@ import './App.css';
 import Flat from './components/Flat';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    }
+  }
+
   render() {
-    const flat = {
-      "id": 148,
-      "name": "Trendy Apt in Buttes Montmartre",
-      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
-      "price": 200,
-      "priceCurrency": "EUR",
-      "lat": 48.885707,
-      "lng": 2.343543
-    };
-
-    const flats = [ flat, flat, flat. flat ];
-
     return (
       <div className="app">
         <div className="main">
           <div className="search">
           </div>
           <div className="flats">
-            {flats.map((flat) => {
+            {this.state.flats.map((flat) => {
               return <Flat flat={flat} />
             })}
           </div>
